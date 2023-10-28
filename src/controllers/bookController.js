abefcf6e64851159944fb562bc0d5fb82189af55
phucphan121200 <phanphuc121200@gmail.com>
2023-10-28 23:27:10 +0700

    delete console log

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -6,9 +6,8 @@ const fs = require('fs');
 
 //ADD FILE BOOK
 exports.addFileBook = async (req, res) => {
-  // try {
+  try {
     const count = await BookModel.count()
-    console.log("COUNT:" +count)
     const workbook = XLSX.readFile(req.file.path);
     const sheet_namelist = workbook.SheetNames;
     let x = 0;
@@ -41,9 +40,9 @@ exports.addFileBook = async (req, res) => {
       }
     })
     res.status(200).json({ success: true, msg: "Thêm đầu sách thành công" })
-  // } catch (error) {
-  //   res.status(200).json({ success: false, data: [], msg: "Lỗi hệ thống" })
-  // }
+  } catch (error) {
+    res.status(200).json({ success: false, data: [], msg: "Lỗi hệ thống" })
+  }
 }
 
 //EXPORT FILE
